7533ed5c82c0f2f738c8588472e1c1899735abe3
yaroslav <yardru@yandex.ru>
2016-08-08 22:40:18 +0300

    add args names

--- mth/matr.h
+++ mth/matr.h
@@ -18,58 +18,56 @@ namespace mth
         protected:
             type _components[m * n]; // components
             // make copy of the matrix
-            matr_t & clone(const matr_t &);
+            matr_t & clone(const matr_t &matr);
         public:
-            // get random matrix
-            static matr_t rand(const type & = type(0), const type & = type(1));
             // constructors
             matr_t();
-            matr_t(const matr_t &);
-            matr_t(const type &);
-            matr_t(const type *);
-            matr_t(const type **);
-            matr_t(type, type, ...);
+            matr_t(const matr_t &matr);
+            matr_t(const type &value);
+            matr_t(const type *values);
+            matr_t(const type **values);
+            matr_t(type value1, type value2, ...);
             // math operators
-            matr_t & operator+=(const matr_t &);
-            matr_t & operator-=(const matr_t &);
-            matr_t & operator*=(const type &);
-            matr_t & operator/=(const type &);
+            matr_t & operator+=(const matr_t &matr);
+            matr_t & operator-=(const matr_t &value);
+            matr_t & operator*=(const type &value);
+            matr_t & operator/=(const type &value);
             // other operators
-            matr_t & operator=(const matr_t &);
-            type & operator()(uint, uint);
-            type operator()(uint, uint) const;
-            type * operator[](uint);
-            const type * operator[](uint) const;
+            matr_t & operator=(const matr_t &matr);
+            type & operator()(uint i, uint j);
+            type operator()(uint i, uint j) const;
+            type * operator[](uint i);
+            const type * operator[](uint i) const;
             // setters
-            matr_t & set(const matr_t &);
-            matr_t & set(const type &);
-            matr_t & set(const type *);
-            matr_t & set(const type **);
-            matr_t & set(type, type, ...);
+            matr_t & set(const matr_t &matr);
+            matr_t & set(const type &value);
+            matr_t & set(const type *values);
+            matr_t & set(const type **values);
+            matr_t & set(type value1, type value2, ...);
             // setters/getters for rows and columns
-            matr_t & set_row(uint, const type &);
-            matr_t & set_row(uint, const type *);
-            type * get_row(uint, type *) const;
-            type * get_row(uint);
-            matr_t & set_col(uint, const type &);
-            matr_t & set_col(uint, const type *);
-            type * get_col(uint, type *) const;
+            matr_t & set_row(uint i, const type &value);
+            matr_t & set_row(uint i, const type *values);
+            type * get_row(uint i, type *to) const;
+            type * get_row(uint i);
+            matr_t & set_col(uint j, const type &value);
+            matr_t & set_col(uint j, const type *values);
+            type * get_col(uint j, type *to) const;
             // transpose matrix
             matr_t<n, m, type> transposing() const; // to other
             // elementary operations
-            matr_t & row_mul(uint, const type &);  // multiply row by number
-            matr_t & row_add_to(uint, const uint,
-                const type & = type(1));           // add scaled second row to first
-            matr_t & row_swap(uint, uint);         // swap two rows
-            matr_t & col_mul(uint, const type &);  // multiply column by number
-            matr_t & col_add_to(uint, const uint,
-                const type & = type(1));           // add scaled second column to first
-            matr_t & col_swap(uint, uint);         // swap two columns
+            matr_t & row_mul(uint i, const type &value);  // multiply row by number
+            matr_t & row_add_to(uint i_to, uint i_from,
+                const type &scale = type(1));           // add scaled second row to first
+            matr_t & row_swap(uint i1, uint i2);         // swap two rows
+            matr_t & col_mul(uint j, const type &value);  // multiply column by number
+            matr_t & col_add_to(uint j_to, uint j_from,
+                const type &scale = type(1));           // add scaled second column to first
+            matr_t & col_swap(uint j1, uint j2);         // swap two columns
             // math functions for square matrix
             matr_t & transpose();
             type trace() const;
-            matr_t<n - 1, n - 1, type> minor(uint, uint) const;
-            type cofactor(uint, uint) const;
+            matr_t<n - 1, n - 1, type> minor(uint i, uint j) const;
+            type cofactor(uint i, uint j) const;
             type det() const;
             matr_t & invert();
             matr_t inverting() const;
@@ -77,57 +75,63 @@ namespace mth
 
     // transpose to other matrix functions
     template <uint m, uint n, typename type>
-        matr_t<n, m, type> transposing(const matr_t<m, n, type> &);
+        matr_t<n, m, type> transposing(const matr_t<m, n, type> &matr);
     // functions for square matrix
     template <uint n, typename type>
-        matr_t<n, n, type> & transpose(matr_t<n, n, type> &);
+        matr_t<n, n, type> & transpose(matr_t<n, n, type> &matr);
     template <uint n, typename type>
-        matr_t<n - 1, n - 1, type> minor(const matr_t<n, n, type> &, uint, uint);
+        matr_t<n - 1, n - 1, type> minor(const matr_t<n, n, type> &matr, uint i, uint j);
     template <uint n, typename type>
-        type det(const matr_t<n, n, type> &);
+        type det(const matr_t<n, n, type> &matr);
     template <uint n, typename type>
-        type cofactor(const matr_t<n, n, type> &, uint, uint);
+        type cofactor(const matr_t<n, n, type> &matr, uint i, uint j);
     template <uint n, typename type>
-        type trace(const matr_t<n, n, type> &);
+        type trace(const matr_t<n, n, type> &matr);
     template <uint n, typename type>
-        matr_t<n, n, type> inverting(const matr_t<n, n, type> &);
+        matr_t<n, n, type> inverting(const matr_t<n, n, type> &matr);
     template <uint n, typename type>
-        matr_t<n, n, type> & invert(matr_t<n, n, type> &);
+        matr_t<n, n, type> & invert(matr_t<n, n, type> &matr);
     template <uint n, typename type>
-        matr_t<n, n, type> inv(const matr_t<n, n, type> &); // same as inverting
+        matr_t<n, n, type> inv(const matr_t<n, n, type> &matr); // same as inverting
     // matrix simple math operators
     template <uint m, uint n, typename type>
-        matr_t<m, n, type> operator-(const matr_t<m, n, type> &);
+        matr_t<m, n, type> operator-(const matr_t<m, n, type> &matr);
     template <uint m, uint n, typename type>
-        matr_t<m, n, type> operator+(const matr_t<m, n, type> &, const matr_t<m, n, type> &);
+        matr_t<m, n, type> operator+(const matr_t<m, n, type> &matr1, const matr_t<m, n, type> &matr2);
     template <uint m, uint n, typename type>
-        matr_t<m, n, type> operator-(const matr_t<m, n, type> &, const matr_t<m, n, type> &);
+        matr_t<m, n, type> operator-(const matr_t<m, n, type> &matr1, const matr_t<m, n, type> &matr2);
     template <uint m, uint n, typename type>
-        matr_t<m, n, type> operator*(const matr_t<m, n, type> &, const type &);
+        matr_t<m, n, type> operator*(const matr_t<m, n, type> &matr, const type &value);
     template <uint m, uint n, typename type>
-        matr_t<m, n, type> operator*(const type &, const matr_t<m, n, type> &);
+        matr_t<m, n, type> operator*(const type &value, const matr_t<m, n, type> &matr);
     template <uint m, uint n, typename type>
-        matr_t<m, n, type> operator/(const matr_t<m, n, type> &, const type &);
+        matr_t<m, n, type> operator/(const matr_t<m, n, type> &matr, const type &value);
     template <uint n, typename type>
-        matr_t<n, n, type> operator/(const type &, const matr_t<n, n, type> &);
+        matr_t<n, n, type> operator/(const type &value, const matr_t<n, n, type> &matr);
     // matrix by matrix multiply operators
     template <uint m, uint k, uint n, typename type>
-        matr_t<m, n, type> operator*(const matr_t<m, k, type> &, const matr_t<k, n, type> &);
+        matr_t<m, n, type> operator*(const matr_t<m, k, type> &matr1, const matr_t<k, n, type> &matr2);
     template <uint n, typename type>
-        matr_t<n, n, type> & operator*=(matr_t<n, n, type> &, const matr_t<n, n, type> &);
+        matr_t<n, n, type> & operator*=(matr_t<n, n, type> &matr1, const matr_t<n, n, type> &matr2);
     // vector by matrix multiply operators
     template <uint m, uint n, typename type>
-        vec_t<m, type> operator*(const matr_t<m, n, type> &, const vec_t<n, type> &);
+        vec_t<m, type> operator*(const matr_t<m, n, type> &matr, const vec_t<n, type> &vec);
     template <uint m, uint n, typename type>
-        vec_t<n, type> operator*(const vec_t<m, type>&, const matr_t<m, n, type> &);
+        vec_t<n, type> operator*(const vec_t<m, type> &vec, const matr_t<m, n, type> &matr);
     template <uint n, typename type>
-        vec_t<n, type> & operator*=(vec_t<n, type> &, const matr_t<n, n, type> &);
-    // matrix division operators (multiply by invert)
+        vec_t<n, type> & operator*=(vec_t<n, type> &vec, const matr_t<n, n, type> &matr);
+    // matrix division operators (multiply by invert) (left - matrix or vector)
     template <typename left_type, uint n, typename type>
-        left_type operator/(const left_type &, const matr_t<n, n, type> &);
+        left_type operator/(const left_type &left, const matr_t<n, n, type> &matr);
     template <typename left_type, uint n, typename type>
-        left_type & operator/=(left_type &, const matr_t<n, n, type> &);
+        left_type & operator/=(left_type &left, const matr_t<n, n, type> &matr);
 }
+
+// iostream input/output definition
+template <uint m, uint n, typename type>
+    std::istream & operator>>(std::istream &is, mth::matr_t<m, n, type> &matr);
+template <uint m, uint n, typename type>
+    std::ostream & operator<<(std::ostream &os, const mth::matr_t<m, n, type> &matr);
 
 #include "matr_imp.h"
 
--- mth/vec.h
+++ mth/vec.h
@@ -21,30 +21,30 @@ namespace mth
             type &s, &t, &p, &q; // references as texture coordinates
         protected:
             // make copy of the vector
-            vec_t & clone(const vec_t &);
+            vec_t & clone(const vec_t &vec);
         public:
             // constructors
             vec_t();
-            vec_t(const vec_t &);
-            vec_t(const type *);
-            vec_t(const type &);
-            vec_t(type, type, ...);
+            vec_t(const vec_t &vec);
+            vec_t(const type &value);
+            vec_t(const type *values);
+            vec_t(type value1, type value2, ...);
             // math operators
-            vec_t & operator+=(const vec_t &);
-            vec_t & operator-=(const vec_t &);
-            vec_t & operator*=(const type &);
-            vec_t & operator*=(const vec_t &); // component-wise product
-            vec_t & operator/=(const type &);
-            vec_t & operator/=(const vec_t &); // component-wise division
+            vec_t & operator+=(const vec_t &vec);
+            vec_t & operator-=(const vec_t &vec);
+            vec_t & operator*=(const type &value);
+            vec_t & operator*=(const vec_t &vec); // component-wise product
+            vec_t & operator/=(const type &value);
+            vec_t & operator/=(const vec_t &vec); // component-wise division
             // other operators
-            vec_t & operator=(const vec_t &);
-            type & operator[](uint);
-            type operator[](uint) const;
+            vec_t & operator=(const vec_t &vec);
+            type & operator[](uint i);
+            type operator[](uint i) const;
             // setters
-            vec_t & set(const vec_t &);
-            vec_t & set(const type *);
-            vec_t & set(const type &);
-            vec_t & set(type, type, ...);
+            vec_t & set(const vec_t & vec);
+            vec_t & set(const type *value);
+            vec_t & set(const type &value);
+            vec_t & set(type value1, type value2, ...);
             // get vector lenght
             type lenght() const;
             // normalize vector
@@ -54,45 +54,45 @@ namespace mth
 
     // math operators
     template <uint n, typename type>
-        vec_t<n, type> operator-(const vec_t<n, type> &);
+        vec_t<n, type> operator-(const vec_t<n, type> &vec);
     template <uint n, typename type>
-        vec_t<n, type> operator+(const vec_t<n, type> &, const vec_t<n, type> &);
+        vec_t<n, type> operator+(const vec_t<n, type> &vec1, const vec_t<n, type> &vec2);
     template <uint n, typename type>
-        vec_t<n, type> operator-(const vec_t<n, type> &, const vec_t<n, type> &);
+        vec_t<n, type> operator-(const vec_t<n, type> &vec1, const vec_t<n, type> &vec2);
     template <uint n, typename type>
-        vec_t<n, type> operator*(const vec_t<n, type> &, const type &);
+        vec_t<n, type> operator*(const vec_t<n, type> &vec, const type &value);
     template <uint n, typename type>
-        vec_t<n, type> operator*(const type &, const vec_t<n, type> &);
+        vec_t<n, type> operator*(const type &value, const vec_t<n, type> &vec);
     template <uint n, typename type>
-        vec_t<n, type> operator*(const vec_t<n, type> &, const vec_t<n, type> &); // component-wise product
+        vec_t<n, type> operator*(const vec_t<n, type> &vec1, const vec_t<n, type> &vec2); // component-wise product
     template <uint n, typename type>
-        type           operator&(const vec_t<n, type> &, const vec_t<n, type> &); // scalar product
+        type           operator&(const vec_t<n, type> &vec1, const vec_t<n, type> &vec2); // scalar product
     template <uint n, typename type>
-        vec_t<n, type> operator/(const vec_t<n, type> &, const type &);
+        vec_t<n, type> operator/(const vec_t<n, type> &vec, const type &value);
     template <uint n, typename type>
-        vec_t<n, type> operator/(const type &, const vec_t<n, type> &);
+        vec_t<n, type> operator/(const type &value, const vec_t<n, type> &vec);
     template <uint n, typename type>
-        vec_t<n, type> operator/(const vec_t<n, type> &, const vec_t<n, type> &); // component-wise division
+        vec_t<n, type> operator/(const vec_t<n, type> &vec1, const vec_t<n, type> &vec2); // component-wise division
     // order operators
     template <uint n, typename type>
-        bool operator==(const vec_t<n, type> &, const vec_t<n, type> &);
+        bool operator==(const vec_t<n, type> &vec1, const vec_t<n, type> &vec2);
     template <uint n, typename type>
-        bool operator!=(const vec_t<n, type> &, const vec_t<n, type> &);
+        bool operator!=(const vec_t<n, type> &vec1, const vec_t<n, type> &vec2);
     // cross product operator for 3d vectors
     template <typename type>
-        vec_t<3, type> & operator%=(const vec_t<3, type> &, const vec_t<3, type> &);
+        vec_t<3, type> & operator%=(const vec_t<3, type> &vec1, const vec_t<3, type> &vec2);
     template <typename type>
-        vec_t<3, type> operator%(const vec_t<3, type> &, const vec_t<3, type> &);
+        vec_t<3, type> operator%(const vec_t<3, type> &vec1, const vec_t<3, type> &vec2);
 }
 
 // vector absolut value
 template <uint n, typename type>
-    type abs(const mth::vec_t<n, type> &);
+    type abs(const mth::vec_t<n, type> &vec);
 // iostream input/output definition
 template <uint n, typename type>
-    std::istream & operator>>(std::istream &is, mth::vec_t<n, type> &);
+    std::istream & operator>>(std::istream &is, mth::vec_t<n, type> &vec);
 template <uint n, typename type>
-    std::ostream & operator<<(std::ostream &os, const mth::vec_t<n, type> &);
+    std::ostream & operator<<(std::ostream &os, const mth::vec_t<n, type> &vec);
 
 #include "vec_imp.h"
 
